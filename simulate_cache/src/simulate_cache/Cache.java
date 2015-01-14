410a4e9fb29c6ebfd291fd46c30af378c65d12fa
CharlesCliff <1150026521@qq.com>
2015-01-15 00:29:48 +0800

    cache define primary 2015.01.15

--- simulate_cache/src/simulate_cache/Cache.java
+++ simulate_cache/src/simulate_cache/Cache.java
@@ -6,10 +6,50 @@ public class Cache {
 	 * */
 	public static final int CACHE_INDEX=128;
 	public static final int CACHE_INTERN=2;
+	public static final int BLOCK_SIZE = 32;
 	/*
 	 * private variables definition
 	 */
-	private Integer[] valid;// valid or not,0 for invalid ,1 for valid
+	private Boolean valid[];// valid or not,0 for invalid ,1 for valid
 	private Integer[] tag;// tag for cache locate
-	private Integer[][] block;//data block
+	private Integer[][][] block;//data block
+	/*
+	 * initial the cache
+	 */
+	public Cache(){
+		valid = new Boolean[CACHE_INDEX];
+		tag = new Integer[CACHE_INDEX];
+		block = new Integer[CACHE_INDEX][CACHE_INTERN][BLOCK_SIZE];
+		for(int i = 0 ; i < CACHE_INDEX;i++){
+			valid[i]=false;
+			tag[0]=0;
+			for (int j = 0 ; j < CACHE_INTERN;j++){
+				for (int k = 0 ; k < BLOCK_SIZE; k++){
+					block[i][j][k]=0x0;
+				}
+			}
+		}
+	}
+	
+	/*
+	 * invalid entire cache
+	 * 
+	 */
+	public Boolean Invalid_Entire_Cache(){
+		for(int i=0;i<valid.length;i++){
+			valid[i]= false;
+		}
+		return true;
+	}
+	/*
+	 * Linefill
+	 * @param:
+	 */
+	public void Linefill(){
+		//@TODO
+		return;
+	}
+	public int read(){
+		return 0;
+	}
 }
