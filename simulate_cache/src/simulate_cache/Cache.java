f26a3c4e54c35e2c75d0d9f2adde8d5f9fbbf568
CharlesCliff <1150026521@qq.com>
2015-01-16 01:26:09 +0800

    2015 1 16

--- simulate_cache/src/simulate_cache/Cache.java
+++ simulate_cache/src/simulate_cache/Cache.java
@@ -7,23 +7,28 @@ public class Cache {
 	public static final int CACHE_INDEX=128;
 	public static final int CACHE_INTERN=2;
 	public static final int BLOCK_SIZE = 32;
+	
 	/*
 	 * private variables definition
 	 */
-	private Boolean valid[];// valid or not,0 for invalid ,1 for valid
-	private Integer[] tag;// tag for cache locate
-	private Integer[][][] block;//data block
+	private short byteoffset,wordoffset;
+	private short index;
+	private int tags;
+	private Boolean[][] valid;// valid or not,0 for invalid ,1 for valid
+	private Integer[][] tag;// tag for cache locate
+	private byte[][][] block;//data block
 	/*
 	 * initial the cache
 	 */
 	public Cache(){
-		valid = new Boolean[CACHE_INDEX];
-		tag = new Integer[CACHE_INDEX];
-		block = new Integer[CACHE_INDEX][CACHE_INTERN][BLOCK_SIZE];
+		valid = new Boolean[CACHE_INDEX][CACHE_INTERN];
+		tag = new Integer[CACHE_INDEX][CACHE_INTERN];
+		block = new byte[CACHE_INDEX][CACHE_INTERN][BLOCK_SIZE];
 		for(int i = 0 ; i < CACHE_INDEX;i++){
-			valid[i]=false;
-			tag[0]=0;
 			for (int j = 0 ; j < CACHE_INTERN;j++){
+			valid[i][j]=false;
+			tag[i][j]=0;
+			
 				for (int k = 0 ; k < BLOCK_SIZE; k++){
 					block[i][j][k]=0x0;
 				}
@@ -36,8 +41,9 @@ public class Cache {
 	 * 
 	 */
 	public Boolean Invalid_Entire_Cache(){
-		for(int i=0;i<valid.length;i++){
-			valid[i]= false;
+		for(int i=0;i<CACHE_INDEX;i++){
+			for (int j = 0; j <CACHE_INTERN;j++)
+			valid[i][j]= false;
 		}
 		return true;
 	}
@@ -49,7 +55,43 @@ public class Cache {
 		//@TODO
 		return;
 	}
-	public int read(){
+	/*
+	 * read
+	 */
+	public int read_cache(){
+		
 		return 0;
+	}
+	
+	/*
+	 * 
+	 */
+	public Boolean write_cache(){
+		return false;
+	}
+	/*
+	 * print cache status information
+	 */
+	public void Print_Cache(){
+		
+		
+	}
+	/*
+	 * 
+	 */
+	public void addressResolve(int addr){
+		if (addr<0){
+			System.out.println("Bad address");
+			return;
+		}
+		
+		addr = addr&0x7fffffff;
+		byteoffset = (short) (addr&0x03);
+		wordoffset = (short)(addr&0x1c);
+		index=(short)(addr&0xfe0);
+		tags = addr&0xfffff000;
+		
+		
+		return;
 	}
 }
